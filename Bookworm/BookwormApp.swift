f79f1381a68f3f4e5d75e5b4bc3682324b06770e
KhoiLe <Khoilecontact@gmail.com>
2021-07-03 10:26:21 +0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by KhoiLe on 03/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
